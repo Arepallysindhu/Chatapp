1c2292864ef2179dbb7e3d071e59faf0096de650
Arepally sindhu <133510826+Arepallysindhu@users.noreply.github.com>
2023-12-02 22:14:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,34 +1,21 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const user1 = prompt('User 1');
-    const user2 = prompt('User 2');
-    let currentUser = user1;
-
-    const chatMessages = document.getElementById('chat-messages');
+function sendMessage() {
     const messageInput = document.getElementById('message-input');
-    const sendButton = document.getElementById('send-button');
-
-    sendButton.addEventListener('click', sendMessage);
-    messageInput.addEventListener('keydown', (event) => {
-      if (event.key === 'Enter') {
-        event.preventDefault();
-        sendMessage();
-      }
-    });
-
-    function sendMessage() {
-      const message = messageInput.value.trim();
-      if (message !== '') {
-        const messageElement = document.createElement('div');
-        messageElement.classList.add('message');
-        messageElement.textContent = `${currentUser}: ${message}`;
-        chatMessages.appendChild(messageElement);
-        messageInput.value = '';
-
-        // Switch user for the next message
-        currentUser = (currentUser === user1) ? user2 : user1;
-
-        // Scroll to the bottom of the chat on new message
-        chatMessages.scrollTop = chatMessages.scrollHeight;
-      }
+    const message = messageInput.value.trim();
+    if (message !== '') {
+      const chatMessages = document.querySelector('.chat-messages');
+      const messageElement = document.createElement('div');
+      messageElement.classList.add('message');
+      messageElement.textContent = message;
+      chatMessages.appendChild(messageElement);
+      messageInput.value = '';
+      chatMessages.scrollTop = chatMessages.scrollHeight;
+    }
+  }
+  
+  document.getElementById('send-button').addEventListener('click', sendMessage);
+  document.getElementById('message-input').addEventListener('keydown', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      sendMessage();
     }
   });
